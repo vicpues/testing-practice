0ade8453f7204150a8009e77626195aa6301f0c7
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-01-08 16:56:18 +0100

    test: Group tests by function

--- testing-practice.test.js
+++ testing-practice.test.js
@@ -4,18 +4,20 @@ it("Works", () => expect(true).toBe(true));
 
 it("Imported", () => expect(capitalize).not.toBeUndefined());
 
-it("Returns a string", () =>
-    expect(typeof capitalize("sandwich")).toBe("string"));
+describe("Capitalize", () => {
+    it("Returns a string", () =>
+        expect(typeof capitalize("sandwich")).toBe("string"));
 
-it("Capitalizes", () => {
-    expect(capitalize("duck")).toBe("Duck");
-    expect(capitalize("mouse")).toBe("Mouse");
-    expect(capitalize("yellow dog")).toBe("Yellow dog");
-});
+    it("Capitalizes", () => {
+        expect(capitalize("duck")).toBe("Duck");
+        expect(capitalize("mouse")).toBe("Mouse");
+        expect(capitalize("yellow dog")).toBe("Yellow dog");
+    });
 
-it("Deals with empty strings", () => expect(capitalize("")).toBe(""));
+    it("Deals with empty strings", () => expect(capitalize("")).toBe(""));
 
-it("Deals with non-string values", () => {
-    expect(capitalize()).toBeUndefined();
-    expect(capitalize(null)).toBeNull();
+    it("Deals with non-string values", () => {
+        expect(capitalize()).toBeUndefined();
+        expect(capitalize(null)).toBeNull();
+    });
 });
